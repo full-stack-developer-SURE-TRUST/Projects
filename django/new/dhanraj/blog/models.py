75bf32850f1b0a0ed543a34c64ed4c12639c59d2
prashantsagar73 <prashant.sagar73@gmail.com>
2021-07-29 20:55:10 +0530

    fetchig data from backend to frontend & models

--- django/new/dhanraj/blog/models.py
+++ django/new/dhanraj/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)    
+    title = models.CharField(max_length=300) 
+    subtitle = models.CharField(max_length=500,default=" ")
+    content = models.TextField(default=" ")
+
+    def __str__(self):
+        return self.title 
+
+
+    
+
