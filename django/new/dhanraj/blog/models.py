b9e78375c5c1c12af1376cbdcbedc7b1dac2fbee
prashantsagar73 <prashant.sagar73@gmail.com>
2021-07-31 20:09:16 +0530

    blog page

--- django/new/dhanraj/blog/models.py
+++ django/new/dhanraj/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.fields import CharField
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,6 +13,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title 
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk': self.pk})      
 
 
     
